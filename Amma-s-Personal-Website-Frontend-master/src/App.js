533c5c557bec2b9f7aae2e567ce00e8cc694fc7e
Dennis2340 <kamaradennis36@gmail.com>
2023-10-01 20:08:47 +0000

    Rounding up

--- Amma-s-Personal-Website-Frontend-master/src/App.js
+++ Amma-s-Personal-Website-Frontend-master/src/App.js
@@ -1,21 +1,14 @@
 import { Routes, Route } from "react-router-dom"
-import { useSelector } from 'react-redux';
-import Admin from "./pages/Admin"
 import MainLayout from "./Layout"
-import SkeletonCard from "./Components/SkeletonCard";
 
 const App = () => {
-  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
-  console.log(isAuthenticated)
   
   return(
   
     <Routes>
          <Route path="/" element={<MainLayout/>}/>
-         <Route path="admin" element={<Admin/>}/>
-         <Route path="load" element={<SkeletonCard/>}/>
     </Routes>
-    
+       
   )
   
 }
